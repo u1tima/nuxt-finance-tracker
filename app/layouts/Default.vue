b1362a947e353352d8d1841a22b8719f24c971fd
Vitali <vit-sema@yandex.ru>
2025-12-17 15:23:59 +0300

    initial commit

---
+++ app/layouts/Default.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="container mx-auto max-w-4xl">
+		<AppHeader />
+		<main class="my-10">
+			<slot />
+		</main>
+	</div>
+</template>
+
+<style lang="scss">
+	body {
+		@apply dark:bg-gray-900 bg-white
+	}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+	import { transactionViewOptions } from '@@/constants';
+
+	const selectedView = ref(transactionViewOptions[1])
+</script>
+
+<template>
+	<section class="flex items-center justify-between mb-10">
+		<h1 class="text-4xl font-extrabold">
+			Summary
+		</h1>
+		<div>
+			<USelectMenu :items="transactionViewOptions"
+						 v-model="selectedView" />
+		</div>
+	</section>
+</template>
