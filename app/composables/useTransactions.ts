bf8324be3cd808197c731b493cd24285d7a6b5e4
Vitali <vit-sema@yandex.ru>
2026-01-20 10:59:04 +0300

    add fake data script

--- app/composables/useTransactions.ts
+++ app/composables/useTransactions.ts
@@ -7,6 +7,7 @@ export const useTransactions = () => {
 		const { data, error } = await supabase
 			.from('transactions')
 			.select()
+			.order('created_at', { ascending: false })
 
 		if (error) throw error
 		
